44a974ca15fa2e9d1166a35e3aa06382d79ff48a
Tobbe <tobbe_stalesjo@hotmail.com>
2019-10-30 19:15:02 +0100

    Lagt till nya bilder och lägg in nya huntingställen på knight

--- js/Knight.js
+++ js/Knight.js
@@ -1,53 +1,115 @@
-class Knight extends Hunting {
+class Knight extends Domer {
 
 
-    constructor() {
-        super();
-    }
+  constructor() {
+    super();
+  }
 
 
 
-    render(html) {  
-        return html`
+  render(html) {
+    return html`
             <section>
             <table>
             <tr>
               <th>Spawn</th>
               <th>Location</th>
-              <th>Level</th>
+              <th>Level 0-100</th>
             </tr>
+            <tr>
+            <td>Coryms</td>
+            <td>Port Hope</td>
+            <td>40</td>
+          </tr>
             <tr>
               <td>Carlin Cults</td>
               <td>Carlin</td>
               <td>60</td>
             </tr>
             <tr>
-              <td>Centro comercial Moctezuma</td>
-              <td>Francisco Chang</td>
-              <td>Mexico</td>
+              <td>Yielothax</td>
+              <td>Farmine</td>
+              <td>80</td>
             </tr>
             <tr>
-              <td>Ernst Handel</td>
-              <td>Roland Mendel</td>
-              <td>Austria</td>
-            </tr>
-            <tr>
-              <td>Island Trading</td>
-              <td>Helen Bennett</td>
-              <td>UK</td>
-            </tr>
-            <tr>
-              <td>Laughing Bacchus Winecellars</td>
-              <td>Yoshi Tannamuri</td>
-              <td>Canada</td>
-            </tr>
-            <tr>
-              <td>Magazzini Alimentari Riuniti</td>
-              <td>Giovanni Rovelli</td>
-              <td>Italy</td>
+              <td>Krailos Nightmares</td>
+              <td>Krailos</td>
+              <td>80</td>
             </tr>
           </table>
+          <br>
+          <br>
+          <table>
+          <tr>
+              <th>Spawn</th>
+              <th>Location</th>
+              <th>Level 100-200</th>
+          </tr>
+          <tr>
+              <td>Wyrms</td>
+              <td>Darashia</td>
+              <td>130</td>
+          </tr>
+          <tr>
+              <td>Minotaur Cults</td>
+              <td>Thais</td>
+              <td>130</td>
+          </tr>
+          <tr>
+              <td>Barkless</td>
+              <td>Ab'Dendriel</td>
+              <td>130</td>
+          </tr>
+          <tr>
+              <td>Edron Hero Cave</td>
+              <td>Edron</td>
+              <td>150</td>
+          </tr>
+          <tr>
+              <td>Glooth Tower</td>
+              <td>Oramond</td>
+              <td>170</td>
+          </tr>
+        </table>
+        <br>
+        <br>
+        <table>
+        <tr>
+              <th>Spawn</th>
+              <th>Location</th>
+              <th>Level 200-300</th>
+        </tr>
+        <tr>
+              <td>Banuta</td>
+              <td>Port Hope</td>
+              <td>200</td>
+        </tr>
+        <tr>
+              <td>Walls</td>
+              <td>Farmine</td>
+              <td>200</td>
+        </tr>
+        <tr>
+              <td>Asuras Palace</td>
+              <td>Port Hope</td>
+              <td>200</td>
+        </tr>
+      </table>
+      <br>
+      <br>
+      <table>
+        <tr>
+              <th>Spawn</th>
+              <th>Location</th>
+              <th>Level 300-400</th>
+        </tr>
+        <tr>
+              <td>Asura Mirrors</td>
+              <td>Port Hope</td>
+              <td>350</td>
+        </tr>
+    </table>
             </section>
         `
-    }
+  }
 }
--- js/Screenshot.js
+++ js/Screenshot.js
@@ -2,9 +2,15 @@ class Screenhot extends Domer {
 
 
     _images = [
-        "/image/Isiki_A.png",
-        "/image/Isiki_SkillUp.png",
-        "/image/Nepozar.png"
+        "/image/Dumpi_Boss (2).png",
+        "/image/Dumpi_Boss.png",
+        "/image/Dumpi_DeathPvE.png",
+        "/image/Dumpi_LevelUp.png",
+        "/image/Dumpi_Lowh.png",
+        "/image/Nosaye_LevelUp.png",
+        "/image/Ruji_LevelUp.png",
+        "/image/Ruji_Low.png",
+        "/image/Tudak_LevelUp.png",
     ];
 
     constructor() {
@@ -21,6 +27,12 @@ class Screenhot extends Domer {
             <img src="${this._images[0]}" alt="Screenshot" height="250" width="33%">
             <img src="${this._images[1]}" alt="Screenshot" height="250" width="33%">
             <img src="${this._images[2]}" alt="Screenshot" height="250" width="33%">
+            <img src="${this._images[3]}" alt="Screenshot" height="250" width="33%">
+            <img src="${this._images[4]}" alt="Screenshot" height="250" width="33%">
+            <img src="${this._images[5]}" alt="Screenshot" height="250" width="33%">
+            <img src="${this._images[6]}" alt="Screenshot" height="250" width="33%">
+            <img src="${this._images[7]}" alt="Screenshot" height="250" width="33%">
+            <img src="${this._images[8]}" alt="Screenshot" height="250" width="33%">
         </div>
         </section>
         `
