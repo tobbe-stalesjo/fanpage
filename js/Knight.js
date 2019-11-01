88476e1fca5cb07d259f312f7cd15ee71e0050bb
Unknown <tobbe_stalesjo@hotmail.com>
2019-11-01 08:40:26 +0100

    Lagt till länkar på huntingplaces

--- js/Knight.js
+++ js/Knight.js
@@ -22,7 +22,7 @@ class Knight extends Domer {
             <td>40</td>
           </tr>
             <tr>
-              <td>Carlin Cults</td>
+              <td><a href ="https://www.youtube.com/watch?v=mbzMSwf8of0">Carlin Cults</a></td>
               <td>Carlin</td>
               <td>60</td>
             </tr>
@@ -32,7 +32,7 @@ class Knight extends Domer {
               <td>80</td>
             </tr>
             <tr>
-              <td>Krailos Nightmares</td>
+              <td><a href="https://www.youtube.com/watch?v=IMkEfsrU4q4">Krailos Nightmares</a></td>
               <td>Krailos</td>
               <td>80</td>
             </tr>
@@ -46,7 +46,7 @@ class Knight extends Domer {
               <th>Level 100-200</th>
           </tr>
           <tr>
-              <td>Wyrms</td>
+              <td><a href="https://www.youtube.com/watch?v=Q2cX9NWmW6E">Wyrms</a></td>
               <td>Darashia</td>
               <td>130</td>
           </tr>
@@ -80,7 +80,7 @@ class Knight extends Domer {
               <th>Level 200-300</th>
         </tr>
         <tr>
-              <td>Banuta</td>
+              <td><a href="https://www.youtube.com/watch?v=OoUyGyenkhE">Banuta</a></td>
               <td>Port Hope</td>
               <td>200</td>
         </tr>
@@ -90,7 +90,7 @@ class Knight extends Domer {
               <td>200</td>
         </tr>
         <tr>
-              <td>Asuras Palace</td>
+              <td><a href="https://www.youtube.com/watch?v=bWOx3XixdPE">Asuras Palace</a></td>
               <td>Port Hope</td>
               <td>200</td>
         </tr>
@@ -104,10 +104,15 @@ class Knight extends Domer {
               <th>Level 300-400</th>
         </tr>
         <tr>
-              <td>Asura Mirrors</td>
+              <td><a href="https://www.youtube.com/watch?v=vSGSCXcobz8">Asura Mirrors</a></td>
               <td>Port Hope</td>
               <td>350</td>
-        </tr>
+              </tr>
+              <tr>
+                    <td><a href="https://www.youtube.com/watch?v=8sJqsPpl1O8&t=396s">Roshamuul West</a></td>
+                    <td>Roshamuul</td>
+                    <td>350</td>
+              </tr>
     </table>
             </section>
         `
